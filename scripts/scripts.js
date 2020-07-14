1f074c2bb49b262faef71d3b011230ae21759062
Jerry Dong <rtdong9421@yahoo.com>
2020-07-13 23:05:07 -0400

    removed parallax in contacts due to Chrome clipping, added fade-in on scroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,16 @@
-// $(() => {
-//   AOS.init();
-// });
+const scrollFadeUp = () => {
+  const fadeUp = document.querySelectorAll('.fadeUp');
+
+  const fadePosition = (el) => {
+    const elementPosition = el.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight * 8 / 9;
+
+    if (elementPosition < screenPosition) {
+      el.classList.add('faded');
+    }
+  }
+
+  fadeUp.forEach(el => fadePosition(el));
+}
+
+document.querySelector('body').addEventListener('scroll', scrollFadeUp)
