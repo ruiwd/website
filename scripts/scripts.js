832dcc3afafb184835c1c165d09d375fc7a7424c
Jerry Dong <rtdong9421@yahoo.com>
2020-07-15 17:05:34 -0400

    mobile menu and minor layout switches

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,11 @@
-const scrollFadeUp = () => {
+const site = {};
+
+site.scrollFadeUp = () => {
   const fadeUp = document.querySelectorAll('.fadeUp');
 
   const fadePosition = (el) => {
     const elementPosition = el.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight * 8 / 9;
+    const screenPosition = window.innerHeight;
 
     if (elementPosition < screenPosition) {
       el.classList.add('faded');
@@ -13,4 +15,23 @@ const scrollFadeUp = () => {
   fadeUp.forEach(el => fadePosition(el));
 }
 
-document.querySelector('body').addEventListener('scroll', scrollFadeUp)
+document.querySelector('body').addEventListener('scroll', site.scrollFadeUp);
+
+site.smoothAnchor = function(e) {
+  e.preventDefault();
+
+  document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', site.smoothAnchor);
+});
+
+site.navToggle = () => {
+  document.querySelectorAll('.navTop.mobile, div.hamburger .line').forEach(line => {line.classList.toggle('active')});
+}
+document.querySelector('div.hamburger').addEventListener('click', site.navToggle);
+
+
